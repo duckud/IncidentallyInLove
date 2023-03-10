a5a268ff2226519e3c808ffd79e993968e042e5d
duckud <akdemir.eslem@hotmail.com>
2023-03-10 10:08:34 +0100

    dialogue system try

---
+++ Incidentally In Love/Assets/startConvo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor; 
+
+public class startConvo : MonoBehaviour
+{
+    //der ganze code ist von https://www.youtube.com/watch?v=HbrNrvX2Ft4&list=PLfRF6lnXtGqjrhzyQhidqMD-shMHGReXi&index=1 
+
+    public NPCConversation myConversation;
+
+    OnLoad(ConversationManager.Instance.StartConversation(myConversation));
+}
